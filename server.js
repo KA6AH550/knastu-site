0676e1253ec0f2b1fc87c898ae38c0ae9acdc0a7
KA6AH KA6AH <boch.leny2003@gmail.com>
2025-06-03 21:33:49 +1000

    moderator-fix-cors

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:8080', 'https://prof-guit.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(bodyParser.json());
 
